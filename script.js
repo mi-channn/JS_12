0afa4bb2698a57acd2adff661cd04eecf632e2b2
mi-channn <hsdmzk0914@gmail.com>
2023-01-15 22:02:37 +0900

    文字列の取得編

---
+++ script.js
@@ -0,0 +1,34 @@
+function pullDown(){
+const pullDownButton = document.getElementById('lists');
+const pullDownParents = document.getElementById('pull-down');
+const pullDownChild = document.querySelectorAll('.pull-down-list');
+const currentList = document.getElementById('current-list');
+
+pullDownButton.addEventListener('mouseover', function() {
+    this.setAttribute('style', 'background-color:blue');
+    console.log('乗った時は青');
+});
+
+pullDownButton.addEventListener('mouseout', function() {
+    this.removeAttribute('style','background-color:red');
+    console.log('外れた時は赤');
+});
+
+pullDownButton.addEventListener('click', function() {
+    if(pullDownParents.getAttribute('style') == 'display:block'){
+        pullDownParents.removeAttribute('style','display:block');
+        console.log('非表示');
+    } else {
+        pullDownParents.setAttribute('style','display:block');
+        console.log('表示');
+    }
+});
+pullDownChild.forEach(function(list){
+    list.addEventListener('click',function(){
+        const value=list.innerHTML
+        currentList.innerHTML = value
+    });
+});
+};
+
+window.addEventListener('load',pullDown);
